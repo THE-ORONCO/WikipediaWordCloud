427cc64a18c25089b9552e5dce1ce49504aebe12
Marco Kovacic <marco@kovacic.dev>
2019-06-11 16:56:17 +0200

    added toRef folder

--- preprocessor.js
+++ preprocessor.js
@@ -2,3 +2,19 @@
 // fuer jede function in apiCall; apicall an crawler an Write
 // Parrallel ? async? sequenziell? 
 // mehrere instanzen von Crawler?
+
+
+//name muss aufbereitet werden bevor dieser in die apiCalls gehen
+// Bill Gates -> Bill_Gates
+//Liste aller Präsidenten der USA -> Liste_aller_Präsidenten_der_USA
+function enhanceTarget(target){
+    // regex / /g, '+' -> / escape Zeichen welches das leerzeichen umschließt; g globale suche; ,'_' -> ersetzt durch _ 
+    var dataName = target.replace(/ /g, '_');
+    return dataName;
+  }
+
+
+function process(trigger){
+    
+
+}
